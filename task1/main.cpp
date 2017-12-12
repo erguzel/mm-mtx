7b16a6e99cfcdd7bdc0497f192737a7a7ba1945e
oe019 <olgunerguezel@uni-rostock.de>
2017-12-12 22:47:51 +0100

    add message logger and global switches

--- task1/main.cpp
+++ task1/main.cpp
@@ -4,11 +4,43 @@
 
 #include <iostream>
 
+#pragma message ("please use -a or -b options for subquestions of task1.")
+#pragma message ("please send -clock flag to enable clock time calculation.")
+
 using namespace std;
 
+//accepted commandline parameters
 
+const string parameters[2] = {"-a", "-b"};
 
-int main() {
-    cout << "Hello, World!" << std::endl;
-    return 0;
+//global switches
+static bool isA = false;
+static bool isB = false;
+
+//--------utilization functions
+
+//writes given message to console or log file. // todo
+void logMessage(string message){
+    cout<< message << endl;
+}
+
+//Parses command line parameters and sets the global switches for run mode.
+void commandLineParser(int arg, char** args){
+    string option = (args[1]);
+    parameters->find("-a");
+    if(option == "a"){
+        isA = true;
+        logMessage("the global switch set to a");
+    }
+    else if (option == "b"){
+        isB = true;
+        logMessage("the global switch set to b" );
+    } else{
+        cout << "this argument is not supported yet" << std::endl;
+    }
+}
+//--------main
+int main(int arg, char** args) {
+    // parse commandline parameters
+    commandLineParser(arg,args);
 }
