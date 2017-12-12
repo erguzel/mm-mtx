bfc93cedb39a8fb43354cadb76b70da7c4efe611
oe019 <olgunerguezel@uni-rostock.de>
2017-12-12 19:56:49 +0100

    initial commit

---
+++ task1/main.cpp
@@ -0,0 +1,14 @@
+// Olgun Ergüzel, 217205614, Computaitonal Physics
+// other users
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+    cout << "Hello, World!" << std::endl;
+    return 0;
+}
