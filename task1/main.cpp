88c33caa92f2e28d014caf991ee79eaca588356d
oe019 <olgunerguezel@uni-rostock.de>
2017-12-20 23:32:50 +0100

    Entry check completed

--- task1/main.cpp
+++ task1/main.cpp
@@ -28,16 +28,17 @@ void logMessage(string message){
     cout<< message << endl;
 }
 
-//-----User defined types
+//-----USER DEFINED TYPES
 
 //initializes the system : parses commandline parameters, sets global switches for the program run mode
-static class initializer{
+class initializer{
 
 public:static void Initialize(int arg, char** args){
 
         commandLineParser(arg, args);
     }
 
+    //configures run structures according to the run mode
 public: static void Configure(){
 
     }
@@ -91,11 +92,138 @@ private:static void commandLineParser(int arg, char** args){
     }
 };
 
+//represents a matrix element (this structure could be used for other tasks as well, since 1 cpp file demanded per task, this struct will be reused for other tasks in repeated core manner.)
+class Entry{
+    int rowInd, colInd;
+    float value;
+public:Entry(int *rowid, int *colid, float *val){
+        rowInd = *rowid ; colInd = *colid; value = *val;
+    }
+
+public:Entry(){
+
+    }
+public:void setRowInd(int *ind){
+        rowInd = *ind;
+    }
+
+public:int* getRowInd(){
+        return &rowInd;
+    }
+
+public:void setColInd(int ind){
+        colInd = ind;
+    }
+
+public:int* getColInd(){
+        return &colInd;
+    }
+
+public:void setValue(float val){
+        value = val;
+    }
+
+public:float* getValue(){
+        return &value;
+    }
+};
+
+//represents a complete matrix (this structure could be used for other tasks as well, since 1 cpp file demanded per task, this struct will be reused for other tasks in repeated core manner.)
+class Matrix{
+    int *rowNum, *colNum;
+    Entry * Entries;
+//constructor with row and column number initializers
+public:Matrix(int* rownum, int* colnum){
+        rowNum = rownum;
+        colNum = colnum;
+    }
+
+public: Matrix (Entry** entries){
+        Entries =  *entries;
+    }
+
+public:void setRowNum(int *rownum){
+        rowNum = rownum;
+    }
+
+public:int* getRowNum(){
+        return rowNum;
+    }
+
+public:void setColNum(int *colnum){
+        colNum = colnum;
+    }
+
+public:int getColNum(){
+        return  *colNum;
+    }
+
+public:void setEntries(Entry** entries){
+        Entries = *entries;
+    }
+
+public:Entry* getEntries(){
+        return Entries;
+    }
+
+};
+
+// OBJECT FACTORY
+
+class Factory{
+
+public: static Entry* CreateEntry(int colid, int rowid, float val){
+        Entry * e = new Entry(&colid,&rowid,&val);
+        return e;
+
+    }
+
+    //creates a matrix object and returns its adress;
+public: static Matrix* CreateMatrix(int rownum, int colnum){
+        Matrix m (&rownum,&colnum);//actual definition in heap with the  adress of parameters
+        return &m;
+    };
+public:static Matrix* CreateMatrix(Entry * Entries){
+        Matrix m2 (&Entries);
+        return & m2;
+    }
+};
+
+//
 
 
 //--------main
 int main(int arg, char** args) {
 
-    initializer in;
-    in.Initialize(arg,args);
+
+    Factory fac;
+    Entry *poent1 = fac.CreateEntry(1,1,1.12);
+    Entry *poent2 = fac.CreateEntry(1,2,1.15);
+    Entry *poent3 = fac.CreateEntry(1,3,1.17);
+
+    //list of entry pointers pointing real value in heap
+    Entry *ents[] = {poent1,poent2,poent3};
+
+    //size of the entry pointer array
+    int siz = sizeof(ents)/ sizeof(*ents);
+
+    //print any value of entries int the entry pointer array
+    for(int i = 0 ; i<siz; i++){
+        cout<< *ents[i]->getValue()<< endl;
+    }
+
+
+    poent1->setValue(3.15);
+
+    cout<<"------------";
+
+    //print any value of entries int the entry pointer array
+    for(int i = 0 ; i<siz; i++){
+        cout<< *ents[i]->getValue()<< endl;
+    }
+
+
+
+    logMessage("stophere");
+
 }
