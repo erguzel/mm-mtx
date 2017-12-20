2765629cbda0fa79a572f9651439a3aee8a2ace3
oe019 <olgunerguezel@uni-rostock.de>
2017-12-20 12:49:07 +0100

    commandline parsing mode set

--- task1/main.cpp
+++ task1/main.cpp
@@ -16,6 +16,10 @@ const string parameters[2] = {"-a", "-b"};
 //global switches
 static bool isA = false;
 static bool isB = false;
+static bool isClock = false;
+//
+
+string option;
 
 //--------utilization functions
 
@@ -24,11 +28,56 @@ void logMessage(string message){
     cout<< message << endl;
 }
 
+static class initializer{
+
+public:static void Initialize(){
+        cout << "system initilzed" << endl;
+    }
+
+public:static void configure(){
+        cout<< "system configured"<< endl;
+    }
+};
+
 //Parses command line parameters and sets the global switches for run mode.
-void commandLineParser(int arg, char** args){
-    string option = (args[1]);
-    parameters->find("-a");
-    if(option == "a"){
+static void commandLineParser(int arg, char** args){
+   // cout<<"the parameter is"<<option<<endl;
+
+    //no parameters provided
+    if (arg == 1){
+        isA = true;
+        isB = true;
+        logMessage("Multimode run is enabled.");
+    }
+
+    // firts argument is the file itself.
+    //check argument number
+    if (arg-1 > 2){
+        logMessage("error.. too may arguments specified. Quitting...");
+        return;
+    }
+    else{
+        for (int i = 1 ; i < 4 ; i++){
+            option = args[i];
+            if(option =="-a"){
+                logMessage("mod a is activated..");
+                isA = true;
+            }
+            else if (option == "-b"){
+                logMessage("mod b is activated..");
+                isB = true;
+            }
+            else{
+                cout<<"the argument " << args[i] << " is not supported yet"<<endl;
+            }
+        }
+    }
+
+
+
+
+
+    if(option == "-a"){
         isA = true;
         logMessage("the global switch set to a");
     }
@@ -42,5 +91,7 @@ void commandLineParser(int arg, char** args){
 //--------main
 int main(int arg, char** args) {
     // parse commandline parameters
+    initializer in;
+    in.Initialize();
     commandLineParser(arg,args);
 }
