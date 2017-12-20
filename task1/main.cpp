9fbd3c847ce450be27a4e191a8c6ca62a0ffcd07
oe019 <olgunerguezel@uni-rostock.de>
2017-12-20 12:59:55 +0100

    some code refactor:

--- task1/main.cpp
+++ task1/main.cpp
@@ -18,7 +18,7 @@ static bool isA = false;
 static bool isB = false;
 static bool isClock = false;
 //
-
+//global variables;
 string option;
 
 //--------utilization functions
@@ -28,70 +28,59 @@ void logMessage(string message){
     cout<< message << endl;
 }
 
+//-----User defined types
+
+//initializes the system : parses commandline parameters, sets global switches for the program run mode
 static class initializer{
 
-public:static void Initialize(){
-        cout << "system initilzed" << endl;
+public:static void Initialize(int arg, char** args){
+
+        commandLineParser(arg, args);
     }
 
-public:static void configure(){
-        cout<< "system configured"<< endl;
-    }
-};
+    //Parses command line parameters and sets the global switches for run mode.
+private:static void commandLineParser(int arg, char** args){
+        // cout<<"the parameter is"<<option<<endl;
 
-//Parses command line parameters and sets the global switches for run mode.
-static void commandLineParser(int arg, char** args){
-   // cout<<"the parameter is"<<option<<endl;
+        //no parameters provided
+        if (arg == 1){
+            isA = true;
+            isB = true;
+            logMessage("Multimode run is enabled.");
+        }
 
-    //no parameters provided
-    if (arg == 1){
-        isA = true;
-        isB = true;
-        logMessage("Multimode run is enabled.");
-    }
-
-    // firts argument is the file itself.
-    //check argument number
-    if (arg-1 > 2){
-        logMessage("error.. too may arguments specified. Quitting...");
-        return;
-    }
-    else{
-        for (int i = 1 ; i < 4 ; i++){
-            option = args[i];
-            if(option =="-a"){
-                logMessage("mod a is activated..");
-                isA = true;
-            }
-            else if (option == "-b"){
-                logMessage("mod b is activated..");
-                isB = true;
-            }
-            else{
-                cout<<"the argument " << args[i] << " is not supported yet"<<endl;
+        // firts argument is the file itself.
+        //check argument number
+        if (arg-1 > 2){
+            logMessage("error.. too may arguments specified. Quitting...");
+            return;
+        }
+        else{
+            for (int i = 1 ; i < 4 ; i++){
+                option = args[i];
+                // if one of the parameters is -a
+                if(option =="-a"){
+                    logMessage("mod a is activated..");
+                    isA = true;
+                }
+                    //if one if the parameters is -b
+                else if (option == "-b"){
+                    logMessage("mod b is activated..");
+                    isB = true;
+                }
+                else{
+                    cout<<"the argument " << args[i] << " is not supported yet"<<endl;
+                }
             }
         }
     }
+};
 
 
 
-
-
-    if(option == "-a"){
-        isA = true;
-        logMessage("the global switch set to a");
-    }
-    else if (option == "b"){
-        isB = true;
-        logMessage("the global switch set to b" );
-    } else{
-        cout << "this argument is not supported yet" << std::endl;
-    }
-}
 //--------main
 int main(int arg, char** args) {
-    // parse commandline parameters
+
     initializer in;
-    in.Initialize();
-    commandLineParser(arg,args);
+    in.Initialize(arg,args);
 }
