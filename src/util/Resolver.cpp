bb1ea862b846716594db7d8a28019707e80f90c4
oe019 <olgun.erguezel@uni-rostock.de>
2018-03-11 21:34:12 +0100

    add global switches to Resolver static class

--- src/util/Resolver.cpp
+++ src/util/Resolver.cpp
@@ -2,4 +2,26 @@
 // Created by Olgun Erguzel on 11.03.2018.
 //
 
-#include "resolver.h"
+#include "Resolver.h"
+
+
+bool::Resolver::isParalell = false;
+
+bool Resolver:: isClock = false;
+
+clock_t Resolver:: start;
+
+clock_t Resolver:: finish;
+
+double Resolver:: progressTime;
+
+std::string Resolver:: option;
+
+double Resolver:: fRand(double fMin, double fMax);
+
+int Resolver:: getIndexFromRowColumn(int rowNumber, int columnNumber, int ColumnSize, int RowSize);
+
+void Resolver:: logMessage(std::string message);
+
+int Resolver:: number_of_threads = 1;
+
--- src/util/Resolver.h
+++ src/util/Resolver.h
@@ -5,4 +5,51 @@
 #ifndef MATRIX_MULTIPLIER_RESOLVER_H
 #define MATRIX_MULTIPLIER_RESOLVER_H
 
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+
+
+static class Resolver {
+
+public:
+    static bool isParalell = false;
+public:
+    static bool isClock = false;
+public:
+    static clock_t start;
+public:
+    static clock_t finish;
+public:
+    double progressTime;
+public:
+    static std::string option;
+public:
+    static double fRand(double fMin, double fMax)
+    {
+        double f = (double)rand() / RAND_MAX;
+        return fMin + f * (fMax - fMin);
+    }
+
+public:
+    static  int getIndexFromRowColumn(int rowNumber, int columnNumber, int ColumnSize, int RowSize) {
+        int currentIndex = (RowSize+((rowNumber-1)*RowSize) - (RowSize-columnNumber))-1;
+        return currentIndex;
+    }
+
+/**
+ * Writes a simple string message to the console
+ * @param message the string consol message
+ */
+public:
+    static  void logMessage(std::string message) {
+        std::cout << message << std::endl;
+    }
+
+public:
+    static int number_of_threads = 1;
+};
+
+
 #endif //MATRIX_MULTIPLIER_RESOLVER_H
